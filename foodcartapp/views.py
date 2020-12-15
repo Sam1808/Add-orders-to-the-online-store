3da8a02bfed798201c3db3685bffc86d1c808e91
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2020-12-15 20:40:43 +0300

    Extend data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -62,28 +62,39 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     order_data = request.data
-    print(order_data)  # delete string
+    print(order_data)  # TODO delete string
 
     try:
         products = order_data['products']
+        firstname = order_data['firstname']
+        lastname = order_data['lastname']
+        phone = order_data['phonenumber']
+        address = order_data['address']
     except KeyError:
-        return Response("There is no 'products'", status=status.HTTP_400_BAD_REQUEST)
+        return Response("Not enough data", status=status.HTTP_400_BAD_REQUEST)
 
-    if not isinstance(products, list) or len(products)==0:
-        return Response(f"{order_data['products']} is not list or null", status=status.HTTP_400_BAD_REQUEST)
+    for value in order_data.values():
+        if value is None or len(value) == 0:
+            return Response("Some data was lost", status=status.HTTP_400_BAD_REQUEST)
 
-    order = CustomerOrder.objects.create(
-        firstname=order_data['firstname'],
-        lastname=order_data['lastname'],
-        phone=order_data['phonenumber'],
-        address=order_data['address'],
+    if not isinstance(products, list) or not isinstance(firstname, str):
+        return Response("Wrong data type", status=status.HTTP_400_BAD_REQUEST)
+
+    order = CustomerOrder.objects.create(  # TODO: move order creation
+        firstname=firstname,
+        lastname=lastname,
+        phone=phone,
+        address=address,
     )
 
     for product in order_data['products']:
+        selected_product = product['product']
+        if not isinstance(selected_product, int):
+            return Response("Wrong product data type", status=status.HTTP_400_BAD_REQUEST)
         order_details = OrderDetails.objects.create(
             quantity=product['quantity']
         )        
-        order_details.product_id = product['product']
+        order_details.product_id = selected_product
         order_details.customer_id = order.id
         order_details.save()
 
